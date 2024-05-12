975e2dc155b06ad526c2f4d2eca7a532bc6fd91a
KeotshepileMaje <125873781+KeotshepileMaje@users.noreply.github.com>
2024-05-12 15:16:26 +0200

    Added images

---
+++ utils/images.js
@@ -0,0 +1,26 @@
+export const iconsImgs = {
+  alert: "/icons/alert.svg",
+  bell: "/icons/bell.svg",
+  bills: "/icons/bills.svg",
+  budget: "/icons/budget.svg",
+  card: "/icons/card.svg",
+  check: "/icons/check.svg",
+  empty_check: "/icons/empty_check.svg",
+  gears: "/icons/gears.svg",
+  home: "/icons/home.svg",
+  menu: "/icons/menu.svg",
+  plane: "/icons/plane.svg",
+  plus: "/icons/plus.svg",
+  report: "/icons/report.svg",
+  search: "/icons/search.svg",
+  user: "/icons/user.svg",
+  wallet: "/icons/wallet.svg",
+  wealth: "/icons/wealth.svg",
+};
+
+export const personsImgs = {
+  person_one: "/images/person_one.jpg",
+  person_two: "/images/person_two.jpg",
+  person_three: "/images/person_three.jpg",
+  person_four: "/images/person_four.jpg",
+};
