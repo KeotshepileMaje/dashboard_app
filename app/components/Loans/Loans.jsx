14c53a5a15214827cf901e67d572d536e81cc3ed
KeotshepileMaje <125873781+KeotshepileMaje@users.noreply.github.com>
2024-05-13 13:37:18 +0200

    Added loans and report section

---
+++ app/components/Loans/Loans.jsx
@@ -0,0 +1,54 @@
+import { iconsImgs } from "../../../utils/images";
+
+const Loans = () => {
+  return (
+    <div className="h-[220px] grid-common ">
+      <div className="grid-c-title">
+        <h3 className="grid-c-title-text">Loans</h3>
+        <button className="grid-c-title-icon">
+          <img src={iconsImgs.plus} />
+        </button>
+      </div>
+
+      <div className="grid-c7-content flex justify-around items-center mt-[-42px] mb-[-128px]">
+        <div className="progress-bar flex justify-center items-center ml-[-46px]">
+          
+          <div className="relative">
+            <svg className="relative w-[210px] h-[210px] rotate-90">
+              <circle cx="105" cy="105" r="50"
+              style={{
+                width: '100%',
+                height: '100%',
+                fill: 'none',
+                stroke: 'var(--clr-jet)',
+                strokeWidth: 20,
+                strokeLinecap: 'round',
+                stroke: 'var(--clr-pumpkin)',
+              }}
+              ></circle>
+            </svg>
+
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+              <h3 className="text-lg">
+                50<span>%</span>
+              </h3>
+            </div>
+          </div>
+        </div>
+
+        <ul>
+          <li className="flex flex-col gap-1 mb-2 text-silver-v1">
+            <span>Savings Target</span>
+            <span className="font-semibold">$ 500,000</span>
+          </li>
+          <li className="flex flex-col gap-1 text-silver-v1">
+            <span>Target Reached</span>
+            <span className="font-semibold">$ 250,000</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Loans;
---
+++ app/components/Report/Report.jsx
@@ -0,0 +1,57 @@
+import { iconsImgs } from "../../../utils/images";
+import { reportData } from "../../data/data";
+
+const Report = () => {
+  return (
+    <div
+      className=" grid-common 
+    grid col-start-1 col-end-3 h-[240px]
+    lg:flex
+    lg:flex-col
+    lg:col-start-3
+    lg:col-end-3
+    lg:h-full
+    "
+    >
+      <div className="grid-c-title">
+        <h3 className="grid-c-title-text">Report</h3>
+        <button className="grid-c-title-icon">
+          <img src={iconsImgs.plus} />
+        </button>
+      </div>
+
+      <div className="flex-1">
+        <div className="grid grid-cols-6 items-stretch h-[100%]">
+          <div className="flex flex-col justify-between p-b-[28px]">
+            <span>100</span>
+            <span>75</span>
+            <span>50</span>
+            <span>25</span>
+            <span>0</span>
+          </div>
+          {reportData.map((report) => (
+            <div className="flex flex-col text-center" key={report.id}>
+              <div
+                className="
+              flex-1 w-[32px] mr-auto mf-auto rounded-lg bg-primaryLight relative overflow-hidden
+              "
+              >
+                <div
+                  className="absolute bottom-0 left-0 w-full h-0 rounded-lg  bg-slate-400"
+                  style={{ height: `${report.value1 !== null ? "40%" : ""}` }}
+                ></div>
+                <div
+                  className="absolute bottom-0 left-0 w-full bg-slate-700 rounded-lg"
+                  style={{ height: `${report.value2 !== null ? "60%" : ""}` }}
+                ></div>
+              </div>
+              <span className="mt-[12px]">Jan</span>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Report
