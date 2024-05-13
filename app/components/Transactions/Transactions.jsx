4894f93d82971a98a6c48351bef035170893ac1a
KeotshepileMaje <125873781+KeotshepileMaje@users.noreply.github.com>
2024-05-13 13:36:50 +0200

    Added transaction section

---
+++ app/components/Transactions/Transactions.jsx
@@ -0,0 +1,43 @@
+import "./Transactions.css";
+import { transactions } from "../../data/data";
+import { iconsImgs } from "../../../utils/images";
+
+const Transactions = () => {
+  return (
+    <div className="grid-common">
+      <div className="grid-c-title">
+        <h3 className="grid-c-title-text">All Transactions</h3>
+        <button className="grid-c-title-icon">
+          <img src={iconsImgs.plus} />
+        </button>
+      </div>
+
+      <div className="grid-content">
+        <div className="flex flex-col">
+          {transactions.map((transaction) => (
+            <div
+              className="grid-item flex justify-between items-center mb-6"
+              key={transaction.id}
+            >
+              <div className=" flex items-center gap-x-4">
+                <div className="w-[40px] h-[40px] rounded-full overflow-hidden img-fit-cover">
+                  <img src={transaction.image} alt="" />
+                </div>
+                <p className="font-semibold">
+                  {transaction.name} <span>{transaction.date}</span>
+                </p>
+              </div>
+              <div className="font-semibold">
+                <span className="text-scarlet font-light mt-2 text-base">
+                  $ {transaction.amount}
+                </span>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Transactions;
