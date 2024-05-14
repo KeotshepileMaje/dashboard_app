18b423085cae04f9729280d9a6b18edacadd4c71
KeotshepileMaje <125873781+KeotshepileMaje@users.noreply.github.com>
2024-05-14 16:57:15 +0200

    Addded utils

---
+++ utils/category.jsx
@@ -0,0 +1,29 @@
+import { AiFillPhone, AiOutlineDesktop, AiOutlineLaptop } from "react-icons/ai";
+import { MdOutlineKeyboard, MdStorefront, MdTv } from "react-icons/md";
+
+export const categories = [
+  {
+    label: "All",
+    icon: MdStorefront,
+  },
+  {
+    label: "Phone",
+    icon: AiFillPhone,
+  },
+  {
+    label: "laptop",
+    icon: AiOutlineLaptop,
+  },
+  {
+    label: "Desktop",
+    icon: AiOutlineDesktop,
+  },
+  {
+    label: "Tv",
+    icon: MdTv,
+  },
+  {
+    label: "Acessories",
+    icon: MdOutlineKeyboard,
+  }
+];
