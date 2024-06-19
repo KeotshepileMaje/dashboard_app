99b294f57f36ab7831ff6465beb14a7c6294b0b3
KeotshepileMaje <125873781+KeotshepileMaje@users.noreply.github.com>
2024-06-19 14:11:46 +0200

    removed bg color

--- app/transactions/layout.jsx
+++ app/transactions/layout.jsx
@@ -2,13 +2,12 @@ import OrdersAdminNav from "../components/orders/OrdersAdminNav";
 
 const ManageOrders = ({children}) => {
   return (
-      <div className="bg-white">
-        <div>
-          <OrdersAdminNav />
-        </div>
-        {children}
+    <div>
+      <div>
+        <OrdersAdminNav />
       </div>
-
+      {children}
+    </div>
   );
 }
  
